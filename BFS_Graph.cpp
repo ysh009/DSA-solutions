cbfdf854bc8df223b840ef5975196934399886a0
Yash Dodiya <106154189+ysh009@users.noreply.github.com>
2023-07-15 17:14:26 +0530

    Create BFS_Graph.cpp

---
+++ BFS_Graph.cpp
@@ -0,0 +1,20 @@
+int vis[V] = {0};
+        vector<int> bfs;
+        vis[0] = 1;
+        queue<int> q;
+        q.push(0);
+        
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            vis[node] = 1;
+            bfs.push_back(node);
+            
+            for(auto it: adj[node]){
+                if(!vis[it]){
+                    vis[it] = 1;
+                    q.push(it);
+                }
+            }
+        }
+        return bfs;
