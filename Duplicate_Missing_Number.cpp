5ef75feca1b0f08b101185f6b6977424c0369144
Yash Dodiya <106154189+ysh009@users.noreply.github.com>
2023-09-05 19:09:37 +0530

    Create Duplicate_Missing_Number.cpp

---
+++ Duplicate_Missing_Number.cpp
@@ -0,0 +1,28 @@
+vector<int> findTwoElement(vector<int> arr, int n) {
+        // code here
+        vector<int> ans;
+        int nsum = 0;
+        int sum = 0;
+        
+        for(int i = 1; i<=n; i++){
+            nsum+=i;
+        }
+        
+        sort(arr.begin(), arr.end());
+        int a = -1;
+        
+        for(int i = 1; i<n; i++){
+            if(arr[i] == arr[i-1]){
+                a = arr[i-1];
+                arr[i-1] = 0;
+            }
+        }
+        ans.push_back(a);
+        for(int i = 0; i<n; i++){
+            sum += arr[i];
+        }
+        ans.push_back(nsum - sum);
+        
+        
+        return ans;
+    }
