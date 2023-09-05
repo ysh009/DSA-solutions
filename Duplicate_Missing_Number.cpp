f505541bf102d8ddd851cb7ca0008ab22de4695b
Yash Dodiya <106154189+ysh009@users.noreply.github.com>
2023-09-05 19:16:25 +0530

    Update Duplicate_Missing_Number.cpp

--- Duplicate_Missing_Number.cpp
+++ Duplicate_Missing_Number.cpp
@@ -1,3 +1,5 @@
+//Here two code solutions are given so look on both
+
 // solution bases on the sorting number
 vector<int> findTwoElement(vector<int> arr, int n) {
         // code here
@@ -26,4 +28,33 @@ vector<int> findTwoElement(vector<int> arr, int n) {
         
         
         return ans;
+    }
+
+//Solution 2 on the basis of the index
+vector<int> findTwoElement(vector<int> arr, int n) {
+        // code here
+        // vector<int> ans;
+        int i1 = 0;
+        int i2 = 0;
+        
+        for(int i = 0; i<n; i++){
+            
+            if(arr[abs(arr[i]) - 1] < 0){
+                i1 = abs(arr[i]);
+            }
+            else{
+                arr[abs(arr[i]) - 1] = -arr[abs(arr[i]) - 1];
+            }
+        }
+        
+        for(int i = 0; i<n; i++){
+            
+            if(arr[i] > 0){
+                i2 = i+1;
+                break;
+            }
+        }
+        
+        
+        return {i1,i2};
     }
